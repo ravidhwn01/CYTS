c6b9691128dc5fbfa103529949b9c04df99cc07f
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-28 18:29:36 +0530

    counting words

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 const setOfWords = [
-    "The his honour and reputation. Everyone has the right against such interference or",
-    "quick brown fox jumps over the lazy dog",
-    "effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law.",
-    "brown is something witch",
-    "No one shall be held in slavery or servitude slavery and the slave trade shall be prohibited in all their forms.",
-    "No one his honour and reputation. Everyone has the right to the protection of the law against such interference or",
+  "The his honour and reputation. Everyone has the right against such interference or",
+  "quick brown fox jumps over the lazy dog",
+  "effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law.",
+  "brown is something witch",
+  "No one shall be held in slavery or servitude slavery and the slave trade shall be prohibited in all their forms.",
+  "No one his honour and reputation. Everyone has the right to the protection of the law against such interference or",
 ];
 console.log(setOfWords);
-const msg = document.getElementById('msg');
-const typeWords = document.getElementById('myWords');
-const btn = document.getElementById('btn');
+const msg = document.getElementById("msg");
+const typeWords = document.getElementById("myWords");
+const btn = document.getElementById("btn");
 let startTime, endTime;
-const playGame = ()=>{
-    let randomNumber = Math.floor(Math.random()*setOfWords.length);
-    console.log(randomNumber);
-    msg.innerText = setOfWords[randomNumber];
-    let date = new Date();
-    startTime = date.getTime();
-    btn.innerText = "Done";
-}
-const endPlay = ()=>{
-    let date = new Date();
-    endTime = date.getTime();
-    let totalTime = ((endTime-startTime)/1000);
-    console.log(totalTime); 
-}
-btn.addEventListener('click', function(){
-    console.log(this);
-    if(this.innerText == 'Start'){
-        typeWords.disabled = false;
-        playGame();
-    }else if(this.innerText == 'Done'){
-        typeWords.disabled = true;
-        btn.innerText = 'Start';
-        endPlay();
-    }
+const playGame = () => {
+  let randomNumber = Math.floor(Math.random() * setOfWords.length);
+  console.log(randomNumber);
+  msg.innerText = setOfWords[randomNumber];
+  let date = new Date();
+  startTime = date.getTime();
+  btn.innerText = "Done";
+};
+const endPlay = () => {
+  let date = new Date();
+  endTime = date.getTime();
+  let totalTime = (endTime - startTime) / 1000;
+  console.log(totalTime);
+  let totalStr = typeWords.value;
+  let wordCount = wordCounter(totalStr);
+  let speed = Math.round((wordCount / totalTime) * 60);
+  let finalMsg = "You typed total words at speed " + speed + " words per minutes ";
+  finalMsg+= compareWords(msg.innerText, totalStr);
+  msg.innerText = finalMsg;
+};
+const compareWords = (str1,str2)=>{
+     
 
-} )
-
+}
+const wordCounter = (str) => {
+    return str.trim().split(/\s+/).length;
+};
+btn.addEventListener("click", function () {
+  console.log(this);
+  if (this.innerText == "Start") {
+    typeWords.disabled = false;
+    playGame();
+  } else if (this.innerText == "Done") {
+    typeWords.disabled = true;
+    btn.innerText = "Start";
+    typeWords.value = '';
+    endPlay();
+  }
+});
