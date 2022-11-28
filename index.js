e33214b139c28c507e5fec74b36f6b3e194bcad3
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-28 19:34:57 +0530

    complete the project

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const setOfWords = [
   "No one shall be held in slavery or servitude slavery and the slave trade shall be prohibited in all their forms.",
   "No one his honour and reputation. Everyone has the right to the protection of the law against such interference or",
 ];
-console.log(setOfWords);
 const msg = document.getElementById("msg");
 const typeWords = document.getElementById("myWords");
 const btn = document.getElementById("btn");
 let startTime, endTime;
 const playGame = () => {
   let randomNumber = Math.floor(Math.random() * setOfWords.length);
-  console.log(randomNumber);
   msg.innerText = setOfWords[randomNumber];
   let date = new Date();
   startTime = date.getTime();
@@ -23,30 +21,42 @@ const endPlay = () => {
   let date = new Date();
   endTime = date.getTime();
   let totalTime = (endTime - startTime) / 1000;
-  console.log(totalTime);
   let totalStr = typeWords.value;
+  console.log(totalStr)
   let wordCount = wordCounter(totalStr);
+  console.log(wordCount)
+
   let speed = Math.round((wordCount / totalTime) * 60);
-  let finalMsg = "You typed total words at speed " + speed + " words per minutes ";
-  finalMsg+= compareWords(msg.innerText, totalStr);
+  let finalMsg = "You typed total words at speed " + speed + " words per minutes. ";
+  finalMsg += compareWords(msg.innerText, totalStr);
   msg.innerText = finalMsg;
 };
 const compareWords = (str1,str2)=>{
-     
+     let words1 = str1.split(" ");
+     let words2 = str2.split(" ");
+     let  count = 0;
+     words1.forEach(function(item, index){
+        if(item == words2[index]){
+            count++;
+        }
+     })
+     let errorWords = (words1.length-count); 
+     return ( count + " correct out of " + words1.length +" words and the total number of error are " + errorWords + "." );
 
 }
 const wordCounter = (str) => {
-    return str.trim().split(/\s+/).length;
+    let res =  str.split(" ").length;
+    console.log(res);
+    return res;
 };
 btn.addEventListener("click", function () {
-  console.log(this);
   if (this.innerText == "Start") {
     typeWords.disabled = false;
     playGame();
   } else if (this.innerText == "Done") {
     typeWords.disabled = true;
     btn.innerText = "Start";
-    typeWords.value = '';
     endPlay();
+    typeWords.value = '';
   }
 });
